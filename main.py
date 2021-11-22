c5cd930e73035ec9eafcf4ca11e7d3b3fcd12dc4
aigul <farkhetdinovaa1@mail.ru>
2021-11-22 20:25:33 +0300

    table ready

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(599, 418)
+        MainWindow.resize(800, 418)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.table_wdg = QtWidgets.QTableWidget(self.centralwidget)
-        self.table_wdg.setGeometry(QtCore.QRect(10, 10, 571, 311))
+        self.table_wdg.setGeometry(QtCore.QRect(10, 10, 750, 311))
         self.table_wdg.setObjectName("tableView")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
@@ -46,7 +46,23 @@ class Espresso(Ui_MainWindow, QMainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.table_wdg.setColumnCount(7)
+        self.table_wdg.setRowCount(4)
+        self.fill_table()
 
+    def fill_table(self):
+        con = sqlite3.connect("coffee (2).sqlite")
+        cur = con.cursor()
+        result = cur.execute("""SELECT * FROM sorts""").fetchall()
+        for i, row in enumerate(result):
+            self.table_wdg.setRowCount(
+                self.table_wdg.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.table_wdg.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+
+    def closeEvent(self, event):
+        self.connection.close()
 
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
